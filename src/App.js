794c11bdbfab44813538d3f1bce3fe8fb75af7d7
Carolina <carolina.torres@creativedrive.com>
2020-03-23 18:40:10 -0600

    Api consumida

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from 'react-dom';
 
-function App() {
+class App extends Component {
+constructor(props){
+    super(props);
+    this.state = {  
+      items: [],
+      isLoaded: false,
+
+    }
+  
+}
+
+componentDidMount() {
+
+  fetch('https://breakingbadapi.com/api/characters')
+  .then(res => res.json())
+  .then(json => {
+    this.setState({
+        isLoaded: true,
+        items: json,
+    })
+  });
+}
+
+
+render() {
+
+  var { isLoaded , items } = this.state;
+
+  if (!isLoaded){
+     
+    return <div>Loading...</div>;
+  }
+
+  else{
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        To get started, edit `src/App.js` and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className= "App">
+     <ul >
+       {items.map(item => (
+        
+       <li key = {item.id}>
+        Name: {item.name} | Birthday: {item.birthday}
+         </li>
+        ))};
+     </ul>
+
     </div>
-  );
+    );
+  }
+}
 }
 
 export default App;
